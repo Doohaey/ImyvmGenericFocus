f986a881ca4644ef7ffe84c2b27b96bafecdbefc
Doohaey Re <doohaey@gmail.com>
2025-08-05 23:51:25 +0800

    Initial Commit.

---
+++ src/client/kotlin/com/imyvm/genefocus/ImyvmGenericFocusClient.kt
@@ -0,0 +1,9 @@
+package com.imyvm.genefocus
+
+import net.fabricmc.api.ClientModInitializer
+
+object ImyvmGenericFocusClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
